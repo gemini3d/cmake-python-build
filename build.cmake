11e19afd7d2427493f67fc1f48bf91f29c533b12
Michael Hirsch <scivision@users.noreply.github.com>
2022-12-21 14:19:07 -0500

    add build.cmake

---
+++ build.cmake
@@ -0,0 +1,34 @@
+cmake_minimum_required(VERSION 3.13)
+
+if(NOT bindir)
+  set(bindir ${CMAKE_CURRENT_LIST_DIR}/build)
+endif()
+get_filename_component(bindir ${bindir} ABSOLUTE)
+
+if(NOT prefix)
+  set(prefix ${bindir}/local)
+endif()
+get_filename_component(prefix ${prefix} ABSOLUTE)
+
+set(conf_args -DCMAKE_INSTALL_PREFIX:PATH=${prefix})
+
+execute_process(COMMAND ${CMAKE_COMMAND}
+-G "Unix Makefiles"
+-S${CMAKE_CURRENT_LIST_DIR}
+-B${bindir}
+${conf_args}
+RESULT_VARIABLE ret
+)
+# Make shows live ExternalProject progress while Ninja is quiet
+if(NOT ret EQUAL 0)
+  message(FATAL_ERROR "Failed to configure")
+endif()
+
+# --- build
+
+execute_process(COMMAND ${CMAKE_COMMAND} --build ${bindir}
+RESULT_VARIABLE ret
+)
+if(NOT ret EQUAL 0)
+  message(FATAL_ERROR "Failed to build / install")
+endif()
