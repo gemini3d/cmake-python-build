60790f4af8f7929af3fd7fe3840f7eba82e98224
scivision <scivision@users.noreply.github.com>
2023-08-28 09:35:31 -0400

    cmake_ignore_prefix

--- options.cmake
+++ options.cmake
@@ -5,7 +5,7 @@ set(CMAKE_TLS_VERIFY ON)
 
 set_property(DIRECTORY PROPERTY EP_UPDATE_DISCONNECTED true)
 
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/cmake)
 
 cmake_host_system_information(RESULT Ncpu QUERY NUMBER_OF_PHYSICAL_CORES)
 
@@ -13,14 +13,9 @@ if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   set(CMAKE_INSTALL_PREFIX "${PROJECT_BINARY_DIR}/local" CACHE PATH "default install prefix" FORCE)
 endif()
 
-# exclude Conda from search
+# exclude Anaconda directories from search
 if(DEFINED ENV{CONDA_PREFIX})
-  set(ignore_path
-    $ENV{CONDA_PREFIX} $ENV{CONDA_PREFIX}/Library $ENV{CONDA_PREFIX}/Scripts $ENV{CONDA_PREFIX}/condabin
-    $ENV{CONDA_PREFIX}/bin $ENV{CONDA_PREFIX}/lib $ENV{CONDA_PREFIX}/include
-    $ENV{CONDA_PREFIX}/Library/bin $ENV{CONDA_PREFIX}/Library/lib $ENV{CONDA_PREFIX}/Library/include
-  )
-  list(APPEND CMAKE_IGNORE_PATH ${ignore_path})
+  list(APPEND CMAKE_IGNORE_PREFIX_PATH $ENV{CONDA_PREFIX})
 endif()
 
 file(GENERATE OUTPUT .gitignore CONTENT "*")
