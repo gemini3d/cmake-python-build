39a52f4b815643f266a96bee644de614e1d6335f
scivision <scivision@users.noreply.github.com>
2023-08-28 22:33:15 -0400

    readline: url

--- readline.cmake
+++ readline.cmake
@@ -11,8 +11,7 @@ if(find)
 endif()
 
 string(JSON readline_url GET ${json_meta} readline url)
-string(JSON readline_tag GET ${json_meta} readline tag)
 
 set(readline_args)
 
-extproj_autotools(readline ${readline_url} ${readline_tag} "${readline_args}")
+extproj_autotools(readline ${readline_url} "" "${readline_args}")
