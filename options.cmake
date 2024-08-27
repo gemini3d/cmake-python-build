fa97ca222aa2dfae278bc645f0248f601450711e
scivision <scivision@users.noreply.github.com>
2024-08-26 20:24:50 -0400

    json default, install prefix test

--- options.cmake
+++ options.cmake
@@ -11,8 +11,14 @@ else()
   cmake_host_system_information(RESULT Ncpu QUERY NUMBER_OF_PHYSICAL_CORES)
 endif()
 
-if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-  message(FATAL_ERROR "Please set cmake --install-prefix to desired install location")
+message(STATUS "CMAKE_INSTALL_PREFIX: ${CMAKE_INSTALL_PREFIX}")
+file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX})
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.29)
+  if(NOT IS_WRITABLE ${CMAKE_INSTALL_PREFIX})
+    message(FATAL_ERROR "CMAKE_INSTALL_PREFIX is not writable: ${CMAKE_INSTALL_PREFIX}")
+  endif()
+else()
+  file(TOUCH ${CMAKE_INSTALL_PREFIX}/.cmake_writable "")
 endif()
 
 # exclude Anaconda directories from search
@@ -31,26 +37,28 @@ file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json json)
 
 # https://www.python.org/downloads/source/
 if(NOT DEFINED python_version)
-  string(JSON python_version GET ${json} "python_version")
+  string(JSON python_version GET ${json} "python")
 endif()
 
+string(JSON bzip_version GET ${json} "bzip")
+
 # https://github.com/libexpat/libexpat/releases
-string(JSON expat_version GET ${json} "expat_version")
+string(JSON expat_version GET ${json} "expat")
 
 # https://github.com/libffi/libffi/releases
-string(JSON ffi_version GET ${json} "ffi_version")
+string(JSON ffi_version GET ${json} "ffi")
 
 # https://github.com/tukaani-project/xz/releases
-string(JSON lzma_version GET ${json} "lzma_version")
+string(JSON lzma_version GET ${json} "lzma")
 
 # https://ftp.gnu.org/gnu/readline/?C=M;O=D
-string(JSON readline_version GET ${json} "readline_version")
+string(JSON readline_version GET ${json} "readline")
 
 # https://github.com/openssl/openssl/releases
-string(JSON ssl_version GET ${json} "ssl_version")
+string(JSON ssl_version GET ${json} "ssl")
 
 # https://github.com/zlib-ng/zlib-ng/releases
-string(JSON zlib_version GET ${json} "zlib_version")
+string(JSON zlib_version GET ${json} "zlib")
 
 if(AUTOCONF_VERSION VERSION_LESS 2.71)
   set(ffi_version "3.4.2")
@@ -63,7 +71,7 @@ string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+" python_url_dir "${python_version}"
 set(python_url https://www.python.org/ftp/python/${python_url_dir}/Python-${python_version}.tar.xz)
 endif()
 
-set(bzip2_url "https://gitlab.com/bzip2/bzip2/-/archive/master/bzip2-master.tar.bz2")
+set(bzip2_url "https://gitlab.com/bzip2/bzip2/-/archive/master/bzip2-${bzip_version}.tar.bz2")
 
 set(expat_url "https://github.com/libexpat/libexpat/archive/refs/tags/R_${expat_version}.tar.gz")
 
